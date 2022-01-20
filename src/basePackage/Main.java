ca34fb9b3ea22a4a71ea1cd99ffc3eca9e8ff4d8
potemkin <tom.alpsten@gmail.com>
2022-01-20 11:26:51 +0100

    Added "Change 1" to the printout

--- src/basePackage/Main.java
+++ src/basePackage/Main.java
@@ -3,6 +3,6 @@ package basePackage;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! Change 1");
     }
 }
