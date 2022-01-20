9e9e43ec43704e9bdd3ce13c7485ae733916b3c6
potemkin <tom.alpsten@gmail.com>
2022-01-20 15:10:39 +0100

    Change 9, the new feature

--- src/basePackage/Main.java
+++ src/basePackage/Main.java
@@ -3,6 +3,6 @@ package basePackage;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world! Change 6&7 = Change 8: \You old son of a bitch!\"");
+        System.out.println("Hello world! Change 9: The New Feature \"So fresh, so clean.\"");
     }
 }
