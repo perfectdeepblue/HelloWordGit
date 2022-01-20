71113164873f4835ef2a51ae0b617d3c0776bbc8
potemkin <tom.alpsten@gmail.com>
2022-01-20 11:27:23 +0100

    Change with Alaba

--- src/basePackage/Main.java
+++ src/basePackage/Main.java
@@ -3,6 +3,6 @@ package basePackage;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world! Change 1");
+        System.out.println("Hello world! Change 1, alaba version");
     }
 }
